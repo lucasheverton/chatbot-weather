1c2fd07cafd692a0fb72d8d9b765bc4767f5c7f4
Lucas Heverton <lucas.7heverton@hotmail.com>
2021-10-31 13:22:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { stripIndent } = require('common-tags');
 const { escape } = require('querystring');
 
-const appid = '590910ac0f90caafb7aadb55aa102485';
+const appid = 'my api key';
 const lang = 'pt_br';
 const unit = 'metric';
 
